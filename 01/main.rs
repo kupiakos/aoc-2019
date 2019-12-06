456590217bc01752f6395c873657415493bbb258
Alyssa Haroldsen <kupiakos@gmail.com>
2019-12-05 19:19:12 -0800

    Day 1

---
+++ 01/main.rs
@@ -0,0 +1,47 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::cmp::max;
+
+fn fuel_for_mass_trivial(mass: i64) -> i64 {
+    max(0, (mass / 3) - 2)
+}
+
+fn fuel_for_mass(mass: i64) -> i64 {
+    let mut sum = 0;
+    let mut stage_mass = mass;
+    loop {
+        stage_mass = fuel_for_mass_trivial(stage_mass);
+        if stage_mass == 0 { break }
+        sum += stage_mass;
+    }
+    sum
+}
+
+fn main() {
+    let file = File::open("01/input.txt").expect("give me input");
+    let masses: Vec<i64> = BufReader::new(file)
+        .lines()
+        .map(|line| line.expect("line error").parse::<i64>().expect("parse error"))
+        .collect();
+    println!("Part 1: {}", masses.iter().copied().map(fuel_for_mass_trivial).sum::<i64>());
+    println!("Part 2: {}", masses.iter().copied().map(fuel_for_mass).sum::<i64>());
+}
+
+mod test {
+    use crate::*;
+
+    #[test]
+    fn trivial_mass() {
+        assert_eq!(fuel_for_mass_trivial(12), 2);
+        assert_eq!(fuel_for_mass_trivial(14), 2);
+        assert_eq!(fuel_for_mass_trivial(1969), 654);
+        assert_eq!(fuel_for_mass_trivial(100756), 33583);
+    }
+
+    #[test]
+    fn full_mass() {
+        assert_eq!(fuel_for_mass(14), 2);
+        assert_eq!(fuel_for_mass(1969), 966);
+        assert_eq!(fuel_for_mass(100756), 50346);
+    }
+}
